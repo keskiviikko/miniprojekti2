d49d157ea4b3437a4f05639e5fee4dd359be9402
Meri Kojonen <merikojonen@gmail.com>
2019-11-20 12:07:10 +0200

    Sivunavigaatiota alettu rakentamaan.

--- src/App.js
+++ src/App.js
@@ -1,39 +1,30 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import '@trendmicro/react-sidenav/dist/react-sidenav.css'
 import Nav from 'react-bootstrap/Nav'
 import './App.css';
 import Home from './components/Home'
 import QuestionForm from './components/QuestionForm'
 import QuestionList from './components/QuestionList'
 import CommentForm from './components/CommentForm'
+import Navigation from './components/Navigation'
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <h2 className='header'>JUUSTO VAI KINKKU?</h2>
-        <Nav className="justify-content-center" activeKey="/">
-          <Nav.Item>
-            <Nav.Link href="/">Home</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link eventKey="link-2" href="/questionlist">Question List</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link eventKey="link-1" href="/questionform">Question Form</Nav.Link>
-          </Nav.Item>
-
-        </Nav>
-        <hr />
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route path='/questionlist' component={QuestionList} />
-          <Route path='/questionform' component={QuestionForm} />
-          <Route path='/commentform' component={CommentForm} />
-        </Switch>
-      </div>
-    </Router>
+    <div className="App">
+      <Router>
+        <Navigation />
+        <div className="MainContent">
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/questionlist' component={QuestionList} />
+            <Route path='/questionform' component={QuestionForm} />
+            <Route path='/commentform' component={CommentForm} />
+          </Switch>
+        </div>
+      </Router>
+    </div>
   );
 }
 
