c03b919dd558f71009c680981c1cee7cbc8db6b9
Hanna Koivisto <hekoivisto@gmail.com>
2019-11-21 13:49:47 +0200

    apiclient.js updated comments, proxy added package.json

--- src/service/apiclient.js
+++ src/service/apiclient.js
@@ -1,20 +1,17 @@
 const url = 'api/questions';
 const url2 = 'api/comments';
-// tuleeko kaikki url:it ja jos tulee, onko sijainnilla väliä? Voi olla samassa
-// const url = 'api/comments';
-// const url = '/';
 
-export const fetchAllQuestions = () => {
+export const getAllQuestions = () => {
     return fetch(url)
         .then(resp => resp.json());
 }
 
-export const fetchSingleQuestion = (id) => {
+export const getSingleQuestion = (id) => {
     return fetch(`${url}/${id}`)
         .then(response => response.json());
 }
 
-export const postQuestion = (question) => {
+export const createQuestion = (question) => {
     return fetch(url, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
@@ -22,21 +19,48 @@ export const postQuestion = (question) => {
     })
 }
 
+export const updateQuestion = (question) => {
+    return fetch(url, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(question)
+    })
+}
 export const deleteSingleQuestion = (id) => {
     return fetch(`${url}/${id}`, {
         method: 'DELETE'
     })
 }
 
-export const fetchAllComments = () => {
+//Comments-osio alkaa tästä:
+
+export const getAllComments = () => {
     return fetch(url2)
         .then(resp => resp.json());
 }
 
-export const postComment = (comment) => {
+export const createComment = (comment) => {
     return fetch(url2, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(comment)
+    })
+}
+
+export const getSingleComment = (id) => {
+    return fetch(`${url2}/${id}`)
+        .then(response => response.json());
+}
+
+export const updateComment = (comment) => {
+    return fetch(url2, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(comment)
+    })
+}
+export const deleteSingleComment = (id) => {
+    return fetch(`${url}/${id}`, {
+        method: 'DELETE'
     })
 }
