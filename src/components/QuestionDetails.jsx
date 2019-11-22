9da1b04e4d81a57f603095a9425ed31351e71ab0
Meri Kojonen <merikojonen@gmail.com>
2019-11-22 12:53:47 +0200

    CSS -tyylejä muokattu. Question details sivulle lisätty Bootstrap gridit.

--- src/components/QuestionDetails.jsx
+++ src/components/QuestionDetails.jsx
@@ -1,8 +1,12 @@
 import React, { Component } from 'react'
 import CommentForm from './CommentForm'
 import CommentList from './CommentList'
+import QuestionList from './QuestionList'
 import { getSingleQuestion } from '../service/apiclient'
 import Poll from 'react-polls'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const pollAnswers = [];
 
@@ -11,10 +15,10 @@ export default class QuestionDetails extends Component {
         pollAnswers: [...pollAnswers],
         question: ''
     }
-    componentDidMount(){
+    componentDidMount() {
         const id = this.props.match.params.id
         getSingleQuestion(id).then(question => {
-            this.setState({question: question[0]})
+            this.setState({ question: question[0] })
         })
     }
     handleVote = voteAnswer => {
@@ -30,18 +34,29 @@ export default class QuestionDetails extends Component {
     render() {
         const { title, details, username } = this.state.question;
         const pollAnswers = [
-            { option: this.state.question.optiona, votes: this.state.question.optionacounter }, 
+            { option: this.state.question.optiona, votes: this.state.question.optionacounter },
             { option: this.state.question.optionb, votes: this.state.question.optionbcounter }
         ]
         return (
-            <div className="Question">
-                <span className="title">{title}</span>
-                <p className="username">{username}</p>
-                <p className="details">{details}</p>
-                <Poll question={title} answers={pollAnswers} onVote={this.handleVote} />
-                <CommentForm/>
-                <CommentList/>
-            </div>
+            // <div className="Question">
+            //     <span className="title">{title}</span>
+            //     <p className="username">{username}</p>
+            //     <p className="details">{details}</p>
+            //     <Poll question={title} answers={pollAnswers} onVote={this.handleVote} />
+            //     <CommentForm />
+
+            // </div>
+            <Container>
+
+            <Row>
+                <Col sm={8}>
+                <h2>Question Details</h2>
+                    <Poll question={title} answers={pollAnswers} onVote={this.handleVote} />
+                    <CommentForm />
+                </Col>
+                <Col sm={4}><QuestionList /></Col>
+            </Row>
+        </Container>
         )
     }
 }
